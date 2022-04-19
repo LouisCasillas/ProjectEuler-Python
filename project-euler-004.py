7780db0592ca23c64b4d3567a01516ccbd125fb2
Louis Casillas <Louis.Casillas@gmail.com>
2022-04-19 11:34:43 -0500

    Removing non-ascii characters

--- project-euler-004.py
+++ project-euler-004.py
@@ -1,5 +1,5 @@
 # Project Euler #4 - https://projecteuler.net/problem=004
-# A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+# A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 x 99.
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
 def is_palindrome(number):
